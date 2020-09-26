47eba87cb4b24e51bf51e6d66fbef94c57280d60
cameronyoung94 <youngcameron0794@gmail.com>
2020-09-25 22:59:30 -0400

    started mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+// import styled from 'styled-components'
+// import reactstrap from 'reactstrap'
+// import bootstrap from 'bootstrap'
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+function App() {
+  const [character, setCharacter] = useState([]);
+  useEffect(() => {
+    axios
+    .get("https://pokeapi.co/api/v2/pokemon/typhlosion")
+    .then(response => {
+      setCharacter(response.data)
+      console.log(response)
+    })
+    .catch((err) => {
+     console.log(err)})
+}, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character name={character.name} sprites={character.sprites} weight={character.weight}/>
     </div>
-  );
-}
-
+    );
+  }
 export default App;
+  
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react';
+// import styled from 'styled-components'
+// import reactstrap from 'reactstrap'
+// import bootstrap from 'bootstrap'
+
+function Character(props) {
+    console.log(props);
+    return (
+        <div>
+            {props.name}
+            <img src= {props.sprites} alt= ''/>
+            <p> {props.weight} </p>
+        </div>
+        )
+   }
+   export default Character
